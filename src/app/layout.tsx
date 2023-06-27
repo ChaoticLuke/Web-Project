e85fa3f3810c2588a7e0d0b188321cf331b18b80
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-27 20:45:32 +0100

    pushing navbar changes v2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import Link from 'next/link'
 import './globals.css'
 import { DM_Sans} from 'next/font/google'
+
 const DM = DM_Sans({
   weight: ["400"],
   subsets: ["latin"],
   fallback: ["system-ui"],
   variable: "--font-DM_Sans",
 })
+
 export const metadata = {
   title: 'NIGHTMARE NIGHTMARE NIGHTMARE',
   description: 'WAKE UP WAKE UP WAKE UP',
 }
+
 export default function RootLayout({
   children,
 }: {
@@ -18,29 +21,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={'${DM.className}'}>
-
-      <body className="bg-white mx-auto">
-        <header className="flex justify-between items-center text-4xl px-10 pb-2">
+      <body className="bg-white mx-auto ">
+        <header className="flex justify-between items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
           <Link href="/">Wessex Footwear</Link>
-          <div className="flex items-center gap-10 text-2xl">
-            <Link href="/about">About us</Link>
-            <p>Third text</p>
+          <div className="flex items-center gap-10 mt-1 font-normal text-2xl">
+            <Link href="/about">About</Link>
+            <Link href="/contact"> Contact Us</Link>
+            <Link href="/allproduct"> All products</Link>
           </div>
-
         </header>
         {children}
       </body>
-
-    
-          
-            
-    
-
-          
-          Expand Down
-    
-    
-  
     </html>
   )
 }
