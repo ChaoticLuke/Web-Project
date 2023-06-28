d2ae3c7155c3224801f010d635ee216d6fcd0cd1
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-06-28 10:30:16 +0100

    Contact us finished (local only)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,18 +2,25 @@ import Image from "next/image"
 
 export default function Contact() {
     return (
-        <div>
-            <Image className='flex block mt-24 ml-10 mr-32 float-right' src="PLACEHOLDER.svg" alt="dash2" width={500} height={1000}/>
-            <h1 className="flex justify-between items-center text-6xl mt-28 mb-1 ml-8 font-medium px-8 pb-2"> Contact Us - </h1>
+        <div className="sans">
+            <Image className='flex mt-24 ml-10 mr-32 float-right font-bold' src="PLACEHOLDER.svg" alt="dash2" width={500} height={1000}/>
+            <h1 className="flex justify-between items-center text-6xl mt-28 mb-1 ml-8 font-medium px-8 pb-2 sans font-bold"> Contact Us - </h1>
             <div>
                 <p className="flex justify-between items-center text-4xl  mb-1 ml-8 font-normal px-10 pb-2"> Intrested? Let's talk. </p>
             </div>
-            <div>
-                <p className="flex justify-between items-center text-3xl mt-20 mb-1 ml-8 font-normal px-10 pb-2"> Buisness Email - Slipper@gmail.com</p>
-                <p className="flex justify-between items-center text-3xl mt-10 mb-1 ml-8 font-normal px-10 pb-2"> Phone Number - 328138921382710839712</p>
-            </div>
-            <Image className='flex fixed bottom-0' src="about_wave.svg" alt="dash2" width={800} height={50}/>
-        </div>
-
+                <div>
+                    <span className="inline-flex items-center mt-5">
+                        <Image className='flex snap-center snap-right float-right rounded-full ml-10 ' src="email_icon.svg" alt="dash2" width={40} height={40}/>
+                        <span className="flex justify-between items-center text-3xl font-normal ml-5"> Buisness Email - Slipper@gmail.com</span>
+                    </span>
+                </div>
+                <div>
+                    <span className="inline-flex items-center mt-5">
+                        <Image className='flex snap-center snap-right float-right rounded-full ml-10 ' src="phone_icon.svg" alt="dash2" width={40} height={40}/>
+                        <span className="flex justify-between items-center text-3xl font-normal ml-5"> Phone Number - +44 1234 5678</span>
+                    </span>
+                </div>
+           <Image className='flex bottom-0 ' src="about_wave.svg" alt="dash2" width={800} height={50}/>
+         </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={'${DM.className}'}>
-      <body className="bg-white mx-auto ">
-        <header className="flex justify-between items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
-          <div className='mt-1'>
+      <body className="bg-white mx-auto sans">
+        <header className=" justify-between flex items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
+          <div className='mt-1 sans font-medium'>
             <Link href="/">Wessex Footwear</Link>
           </div>
-          <div className="flex items-center gap-10 mt-1 font-normal text-2xl">
+          <div className="flex items-center gap-10 mt-1 font-normal sans text-2xl">
             <Link href="/about">About</Link>
             <Link href="/contact"> Contact Us</Link>
             <Link href="/allproduct"> All products</Link>
