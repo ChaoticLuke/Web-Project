0ca02679f819173b2f2d9871d807fd75929075d6
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-03 18:31:17 +0100

    Pushed changes, but unfinished rn

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ export default function Contactus() {
             <Image className='flex snap-center snap-right float-right ml-10 mr-20' src="placeholder_about.svg" alt="dash2" width={700} height={40}/>
             <div>
                 <span className="flex ml-20 items-center text-3xl mt-5 sans">
-                    <p className=""> Hello! This is wessex footwares about page. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    <p> Hello! This is wessex footwares about page. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
                     sunt in culpa qui officia deserunt mollit anim id est laborum.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,15 @@ export default function Home() {
       <div>
         <Image src="Wave.svg" alt="wave" width={1920} height={1080}/>
       </div>
-      <div className="boxedback border-8 right-1 border-green-700 m-5">
-        <Image className="flex justify-end right-1 " src="swoopy_1.svg" alt="wave" width={500} height={1080}/>
+      <div className='m-10 border-4 overflow-visible'>
+       <Image className='float-right m-4' src="placeholder_about.svg" alt="dash2" width={700} height={400}/>
+       <h1 className="text-left text-6xl mt-28 mb-1 ml-1 font-light px-8">Look at my</h1>
+       <h1 className="text-left text-8xl mb-1 ml-1 font-bold px-8">Slippers</h1>
+       <p className="text-left text-xl mt-28 mb-1 ml-1 font-light px-8">The slippers from the compant that exists in the real world 
+       <p className='mt-10'>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                     sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></p>
       </div>
      </div> 
   )
