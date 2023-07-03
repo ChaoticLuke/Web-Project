59728133bde550aebbd74ca6ca4e88753a56bf87
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-03 16:12:45 +0100

    Started doing About us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,24 @@
-export default function About() {
-    return (
-        <div>
-            <h1 className="flex justify-between items-center text-5xl mt-20 mb-1 ml-6 font-medium px-10 pb-2">About us -</h1>
-            <div>
-                <p className="flex justify-between items-center text-3xl mt-10 mb-1 ml-10 font-normal px-10 pb-2">What if idk i told you that we might, in fact, bare with me with this one, its quite a lot to take on, no actually alot to take in, im sorry my grammer seems to be incorrect- if we in fact, perchance, sold slippers.</p>
-            </div>
-        </div>
+import Image from 'next/image'
 
+export default function Contactus() {
+    return (
+        <div className="sans">
+            <div className='ml-10'>
+              <h1 className="flex justify-between items-center text-6xl mt-28 mb-1 ml-1 font-bold px-8">About Us -</h1>
+            </div>
+            <Image className='flex snap-center snap-right float-right ml-10 mr-20' src="placeholder_about.svg" alt="dash2" width={700} height={40}/>
+            <div>
+                <span className="flex ml-20 items-center text-3xl mt-5 sans">
+                    <p className=""> Hello! This is wessex footwares about page. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    <p className='mt-10'>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                     sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> </p>
+                </span>
+            </div>
+         </div>        
     )
 }
--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -5,20 +5,20 @@ export default function Contactus() {
         <div className="sans">
             <Image className='flex mt-30 ml-8 mr-32 float-right font-bold' src="PLACEHOLDER.svg" alt="dash2" width={500} height={1000}/>
             <div className='ml-10'>
-              <h1 className="flex justify-between items-center text-6xl mt-28 mb-1 ml-1 font-medium px-8 pb-2 sans">Contact Us-</h1>
+              <h1 className="flex justify-between items-center text-6xl mt-28 mb-1 ml-1 font-bold px-8 pb-2 sans">Contact Us -</h1>
               <div>
-                <p className="flex justify-between items-center text-4xl  mb-1 font-normal px-10 pb-2">Intrested? Lets talk.</p>
+                <p className="flex justify-between items-center text-4xl  mb-1 font-normal px-10 pb-2">Interested? Let&apos;s talk.</p>
               </div>
               <div>
                 <span className="inline-flex items-center mt-7">
                     <Image className='flex snap-center snap-right float-right rounded-full ml-10 ' src="email_icon.svg" alt="dash2" width={40} height={40}/>
-                    <span className="flex justify-between items-center text-3xl font-normal ml-5">Buisness Email - wessexfootwear@btinternet.com</span>
+                    <span className="flex justify-between items-center text-3xl font-normal ml-5">Business email - wessexfootwear@btinternet.com</span>
                 </span>
               </div>
               <div>
                 <span className="inline-flex items-center mt-5">
                     <Image className='flex snap-center snap-right float-right rounded-full ml-10 ' src="phone_icon.svg" alt="dash2" width={40} height={40}/>
-                    <span className="flex justify-between items-center text-3xl font-normal ml-5">Phone Number - +44 01278723495</span>
+                    <span className="flex justify-between items-center text-3xl font-normal ml-5">Phone number - +44 01278723495</span>
                 </span>
                </div>
              </div>
