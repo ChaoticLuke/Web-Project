189ccc6a9ad1a984bebad9690408b7442ad33877
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-07 15:00:41 +0100

    crying still

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white mx-auto sans">
-        <header className="justify-between flex items-center bg-white pb-1 sm:pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
-          <div className='mt-1 text-xl sm:text-3xl px-3 sm:px-10 sans font-medium float-left'>
+        <header className="justify-between flex bg-white pb-1 sm:pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
+          <div className='mt-1 text-lg sm:text-3xl px-2 sm:px-10 sans font-medium float-left'>
             <Link href="/">Wessex Footwear</Link>
           </div>
-          <div className=" mt-1 flex sm:items-center px-3 sm:px-10 gap-1 sm:gap-10 font-normal sans text-lg sm:text-2xl">
+          <div className=" mt-2 sm:mt-1 text-sm sm:text-1xl flex sm:items-center px-3 sm:px-10 gap-1 sm:gap-10 font-normal sans sm:text-2xl">
             <Link href="/about">About</Link>
             <Link href="/contactus">Contact Us</Link>
             <Link href="/allproducts">All items</Link>
