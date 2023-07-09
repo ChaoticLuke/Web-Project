6498476ffe1fd6afd6e960be295141c3f15a0069
Luke <Lseward10@gmail.com>
2023-07-09 05:10:42 +0100

    added font files to public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import './globals.css'
-import { DM_Sans} from 'next/font/google'
+import { DM_Sans} from "next/font/google"
 
 const DM = DM_Sans({
   weight: ["400"],
@@ -23,19 +23,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={'${DM.className}'}>
-      <body className="bg-white mx-auto sans">
-        <header className=" justify-between flex items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
-          <div className='mt-1 sans font-medium'>
-            <Link href="/">Wessex Footwear</Link>
-          </div>
-          <div className="flex items-center gap-10 mt-1 font-normal sans text-2xl">
-            <Link href="/allproducts">All Products</Link>
-            <Link href="/contactus">Contact Us</Link>
-            <Link href="/about">About</Link>
-          </div>
-        </header>
-        {children}
-      </body>
+      <head className= ''>
+        <body className="bg-white mx-auto sans">
+          <header className=" justify-between flex items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
+            <div className='mt-1 sans font-medium'>
+              <Link href="/">Wessex Footwear</Link>
+            </div>
+            <div className="flex items-center gap-10 mt-1 font-normal sans text-2xl">
+              <Link href="/allproducts">All Products</Link>
+              <Link href="/contactus">Contact Us</Link>
+              <Link href="/about">About</Link>
+            </div>
+          </header>
+          {children}
+        </body>
+      </head>
     </html>
   )
 }
