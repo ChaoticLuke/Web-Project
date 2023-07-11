a5bb0937428a80d4eddf5f7248780f84968363bf
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-11 15:38:30 +0100

    update main mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
       <div>
         <div className='bg-[url("/Wave-main.svg")] pt-20 bg-local bg-center bg-cover bg-no-repeat max-w-full'>
           <div className=' sm:pl-10 text-center object-center'>
-            <h1 className="text-7xl lg:text-8xl mb-1 pt-20 ml-1 font-bold px-8 text-center sm:text-left">Hello!</h1>
-            <h1 className="text-6xl mb-1 ml-1 font-light px-8 text-center sm:text-left">Welcome to Wessex Footwear</h1>
-            <p className='text-5xl ml-1 pt-20 font-light px-8 text-center sm:text-left'>We make high quality, handmade slippers</p>
+            <h1 className="text-6xl sm:text-7xl lg:text-8xl mb-1 pt-20 ml-1 font-bold px-8 text-center sm:text-left">Hello!</h1>
+            <h1 className="text-5xl sm:text-6xl mb-1 ml-1 pt-3 sm:pt-0 font-light px-8 text-center sm:text-left">Welcome to Wessex Footwear</h1>
+            <p className='text-4xl sm:text-5xl ml-1 pt-6 sm:pt-20 font-light px-8 text-center sm:text-left'>We make high quality, handmade slippers</p>
             <div className='sm:justify-between flex content-center justify-center items-center font-medium px-10 pb-2 bottom-0 w-full'>
               <a href="/allproducts" className="top btn3 mt-5 mb-96 text-2xl bg-yellow-500 border-4 border-green-600">View all</a>
             </div>
@@ -19,7 +19,7 @@ export default function Home() {
         <div className='lg:m-5 mt-5 bg-gray-200 border-8 rounded-lg border-green-700 flex'>
           <div>
           <Image className='float-right my-5 lg:m-4 p-5' src="placeholder_about.svg" alt="dash2" width={400} height={400}/>
-            <h1 className="text-6xl mb-1 ml-1 mt-10 font-light px-8 pt-10 text-center sm:text-left">Look at my<br/>
+            <h1 className="text-5xl sm:text-6xl mb-1 ml-1 mt-10 font-light px-8 pt-10 text-center sm:text-left">Look at my<br/>
             <span className="text-7xl lg:text-8xl mb-1 font-bold text-center sm:text-left">Slippers</span></h1>
             <p className="text-left text-3xl mt-14 ml-1 font-light px-8">The slippers from the compant that exists in the real world</p>
             <p className='m-10 text-2xl'>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -32,7 +32,7 @@ export default function Home() {
          <div className='lg:m-5 mt-5 bg-gray-200 border-8 rounded-lg border-yellow-600 flex'>
           <div>
           <Image className='float-right my-5 lg:m-4 p-5' src="placeholder_about.svg" alt="dash2" width={400} height={400}/>
-            <h1 className="text-6xl mb-1 ml-1 mt-10 font-light px-8 pt-10 text-center sm:text-left">Look at my<br/>
+            <h1 className="text-5xl sm:text-6xl mb-1 ml-1 mt-10 font-light px-8 pt-10 text-center sm:text-left">Look at my<br/>
             <span className="text-7xl lg:text-8xl mb-1 font-bold text-center sm:text-left">Slippers</span></h1>
             <p className="text-left text-3xl mt-14 ml-1 font-light px-8">The slippers from the compant that exists in the real world</p>
             <p className='m-10 text-2xl'>This is a paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing
@@ -43,7 +43,7 @@ export default function Home() {
 
         <div className='mt-14'>
           <div className='text-center mb-20'>
-            <h1 className='text-4xl font-bold mb-5 '>See all products</h1>
+            <h1 className='text-3xl sm:text-4xl font-bold mb-5 '>See all products</h1>
             <a href="/allproducts" className='btn p-3 pr-16 pl-16 sm:pr-20 sm:pl-20 text-2xl bg-green-600 border-4 border-gray-700'>click here</a>
           </div>
           <div className='sm:justify-between justify-center flex items-center bg-gray-300 text-2xl font-medium px-10 pb-2 border bottom-0 w-full'>
