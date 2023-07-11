c06a740a30f5231a6eebcabf02b90db2b25866b1
luke <lseward10@gmail.com>
2023-07-11 11:57:14 +0100

    google maps set up aswell as color profiles

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Contactus() {
     return (
-        <div className="sans">
-            <Image className='flex mt-30 ml-8 mr-32 float-right font-bold' src="PLACEHOLDER.svg" alt="dash2" width={500} height={1000}/>
+        <div>
+            <div className='google-map'>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d625.6760995511692!2d-2.8742742050425307!3d51.150801998800844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1689070444942!5m2!1sen!2suk" width="600" height="450" className="flex mt-30 ml-8 mr-32 float-right font-bold border-8 rounded-xl border-peachy"  loading="lazy" ></iframe>            
+            </div>
             <div className='ml-10'>
               <h1 className="justify-between items-center text-6xl mt-28 mb-1 ml-1 font-bold px-8 pb-2">Contact<span className='ml-3 font-bold text-green-700 '>-</span></h1>
               <p className="justify-between items-center text-4xl  mb-1 font-normal px-10 pb-2">Interested?<span className='ml-3 font-medium '>Let&apos;s talk.</span></p>
@@ -20,7 +22,7 @@ export default function Contactus() {
                 </span>
                </div>
              </div>
-           <Image className='object-contain' src="about_wave.svg" alt="dash2" width={800} height={50}/>
+           <Image className='bottom-full' src="about_wave.svg" alt="dash2" width={800} height={50}/>
          </div>             
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+        colors: {
+          'peachy': '#EB826B',
+          'peachylight': '#F4AC84',
+          'pissgreen': '#C3CAA7',
+          'blueygreen': '#64A49D',
+          'mossygreen': '#468484',
+          'darkmoss': '#3D837B',
+        },
         fontFamily: {
           dm_sans: ["var(--font-DM_Sans)"]
         },
