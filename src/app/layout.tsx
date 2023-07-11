19e735a803d2d0bd7efb391dbbb09cf2edb761d8
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-11 15:22:26 +0100

    updates to mobile vers (main)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={'${DM.className}'}>
       <body className="bg-white mx-auto sans">
-        <header className=" justify-between flex items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
-          <div className='mt-1 sans font-medium'>
+        <header className="justify-between flex items-center bg-white text-3xl font-medium px-3 sm:px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
+          <div className='mt-2 text-lg sm:text-3xl px-2 font-medium float-left'>
             <Link href="/">Wessex Footwear</Link>
           </div>
-          <div className="flex items-center gap-10 mt-1 font-normal sans text-2xl">
+          <div className="mt-2 text-sm sm:text-1xl flex sm:items-center px-1 sm:px-10 gap-1 sm:gap-10 font-normal sm:text-2xl">
             <Link href="/allproducts">All Products</Link>
             <Link href="/contactus">Contact Us</Link>
             <Link href="/about">About</Link>
