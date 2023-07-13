1dbbff755713874cfcfd0c7ea93d3527b3247658
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-13 11:46:28 +0100

    fixing mobile version for the third time. yay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={DM.className}>
       <body className="bg-white mx-auto">
-        <header className=" justify-between flex items-center bg-white text-3xl font-medium px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
-          <div className='mt-1 font-medium'>
+        <header className="justify-between flex items-center bg-white text-3xl font-medium px-1 sm:px-10 pb-2 border border-t-0 border-r-0 border-l-0 border-b-2 fixed top-0 w-full">
+          <div className='mt-0.5 sm:mt-2 text-sm sm:text-3xl px-1 font-medium float-left'>
             <Link href="/">Wessex Footwear</Link>
           </div>
           <div className="mt-0.5 sm:mt-2 text-sm flex sm:items-center px-1 sm:px-10 gap-1 sm:gap-10 font-normal sm:text-2xl">
