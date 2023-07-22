01ea50ae8f8822dfce70734d63e8182b20c96394
Luke <Lseward10@gmail.com>
2023-07-22 21:28:54 +0100

    trying to fix stuff still might break sorry

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
   theme: {
     
     extend: {
-<<<<<<< HEAD
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
@@ -20,16 +19,6 @@ module.exports = {
         moistgreen: '#468484',
         padawongreen: '#3D837B',
       },
-=======
-        colors: {
-          'peachy': '#EB826B',
-          'peachylight': '#F4AC84',
-          'pissgreen': '#C3CAA7',
-          'blueygreen': '#64A49D',
-          'mossygreen': '#468484',
-          'darkmoss': '#3D837B',
-        },
->>>>>>> e368337bab58533bbf1dcb97101585e8ca9e196e
         fontFamily: {
           dm_sans: ["var(--font-DM_Sans)"]
         },
