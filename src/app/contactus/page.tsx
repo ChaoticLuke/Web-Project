65c91120f2c65c0fd37d38f8cdf2adbc62cbd224
ScarlettLarder <alarder@student.strode-college.ac.uk>
2023-07-22 15:37:55 +0100

    insane

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -4,7 +4,13 @@ export default function Contactus() {
     return (
         <div>
             <div className='google-map'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d625.6760995511692!2d-2.8742742050425307!3d51.150801998800844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1689070444942!5m2!1sen!2suk" width="600" height="450" className="flex mt-30 ml-8 mr-32 float-right font-bold border-8 rounded-xl border-peachy"  loading="lazy" ></iframe>            
+            <iframe 
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d625.6760995511692!2d-2.8742742050425307!3d51.150801998800844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1689070444942!5m2!1sen!2suk" 
+              width="600" 
+              height="450" 
+              className="flex mt-30 ml-8 mr-32 float-right font-bold border-8 rounded-xl border-peachy"  
+              loading="lazy" >
+              </iframe>            
             </div>
             <div className='ml-10'>
               <h1 className="justify-between items-center text-6xl mt-28 mb-1 ml-1 font-bold px-8 pb-2">Contact<span className='ml-3 font-bold text-mossygreen '>-</span></h1>
