f1b7ffba8ab50b3dc017463d1c4c9e756a4da73a
Luke <Lseward10@gmail.com>
2023-07-24 15:43:54 +0100

    fixing colours

--- src/app/allproducts/page.tsx
+++ src/app/allproducts/page.tsx
@@ -79,7 +79,7 @@ const staticProducts = [
 export default function Allproducts() {
     return (
         <div className="mt-20 ">
-            <h1 className="justify-between items-center text-5xl lg:text-6xl sm:mt-24 mb-1 ml-1 font-bold px-2 sm:px-8 pb-2">All products<span className='ml-3 font-bold text-mossygreen '>-</span></h1>
+            <h1 className="justify-between items-center text-5xl lg:text-6xl sm:mt-24 mb-1 ml-1 font-bold px-2 sm:px-8 pb-2">All products<span className='ml-3 font-bold text-mossgreen '>-</span></h1>
             <h1 className="justify-between items-center text-1xl sm:text-2xl ml-1 px-2 sm:px-8 pb-2">Look through all of our products, with prices per item. Please contact for more info on each.</h1>
             <div className="max-w-2xl mx-auto py-10 lg:max-w-7xl">
                 <div className="grid grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-8">
@@ -102,7 +102,7 @@ export default function Allproducts() {
                 </div>
             </div>
             <div className='sm:justify-between justify-center flex items-center bg-gray-300 text-2xl font-medium px-10 pb-2 border bottom-0 w-full'>
-                <a href="#" className="top btn2 mt-2 text-xl bg-mossygreen border-2 border-gray-700">Go back to the top <span className='ml-2 font-bold text-green-900 '>^</span></a>
+                <a href="#" className="top btn2 mt-2 text-xl bg-mossgreen border-2 border-gray-700">Go back to the top <span className='ml-2 font-bold text-green-900 '>^</span></a>
             </div>
         </div>             
     )
