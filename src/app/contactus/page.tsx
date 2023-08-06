0050301f92d7c8546465e00e6e0b7678fa652ea9
Luke <Lseward10@gmail.com>
2023-08-06 19:06:11 +0100

    blah blah blah

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -6,7 +6,7 @@ export default function Contactus() {
             <div className='google-map mapname '>
               <iframe 
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d625.6760995511692!2d-2.8742742050425307!3d51.150801998800844!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1689070444942!5m2!1sen!2suk" 
-              className="flex m-10 mt-30  border-8 rounded-xl border-peach fixed mb-5 bottom-0 md:top-24 md:right-0 h-2/5 w-4/5 md:w-3/5"  
+              className="flex m-10 mt-30  border-8 rounded-xl border-peach fixed mb-5 bottom-0 md:top-24 md:right-0 h-2/5 w-4/5 md:w-3/5 md:h-3/5"  
               loading="lazy" >
               </iframe>            
             </div>
