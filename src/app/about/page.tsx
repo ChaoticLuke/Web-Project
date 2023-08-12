4edcd35ecf80d00787012dbcf9c6c11a98482636
Luke <Lseward10@gmail.com>
2023-08-12 17:15:32 +0100

    ahhhh

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,8 +8,7 @@ export default function About() {
                 <h1 className="justify-between text-center md:text-left text-3xl md:text-6xl mt-20 md:mt-28 mb-1 font-bold md:ml-1 md:px-8">About us<span className='ml-3 font-bold text-mossgreen '> -</span></h1>     
             </div>
             <div className='m-10'>
-                {/* This image is used in the google maps, and is free to use for commerical use with no attribution required */}
-                <img className=' lg:float-right sm:ml-10 sm:mr-20 sm:w-3/5 lg:w-2/5 lg:h-2/5 sm:shadow-lg border-8 border-mossgreen rounded-xl' loading='lazy' src="\Sheo_9_GreySlipOns.jpeg" alt="Tan scuffs"></img>
+                <img className=' lg:float-right sm:ml-10 sm:mr-20 sm:w-3/5 sm:h-3/5 lg:w-2/5 lg:h-2/5  sm:shadow-lg border-8 border-mossgreen rounded-xl' loading='lazy' src="\Sheo_9_GreySlipOns.jpeg" alt="Tan scuffs"></img>
             </div>
             <div className='m-10 text-center md:text-left text-2xl sm:text-4xl mt-10'>
                 <p className=''>
