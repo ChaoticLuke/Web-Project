303da3e5e9b22f73948237ee7b4618dd5f7c1ffa
Luke <Lseward10@gmail.com>
2023-08-12 16:42:01 +0100

    Typo correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <h2 className="text-5xl sm:text-6xl mb-1 ml-1 pt-3 sm:pt-0 font-light px-8 text-center sm:text-left">Welcome to Wessex Footwear</h2>
             <p className='text-4xl sm:text-5xl ml-1 pt-6 sm:pt-7 font-light px-8 text-center sm:text-left'>We make high quality, handmade slippers</p>
             <button className='sm:justify-between flex content-center justify-center items-center font-medium sm:mx-10 mb-96 mx-auto '>
-              <a href="/showcase" className="top rounded-full font-bold py-2 px-12 cursor-pointer tracking-wide uppercase shadow-2xl mt-5 text-2xl duration-75 delay-50 hover:text-light hover:bg-lpeach bg-peach border-4 border-mossgreen focus:border-ltmossgreen shadow-xl">View all</a>
+              <a href="/showcase" className="top rounded-full font-bold py-2 px-12 cursor-pointer tracking-wide uppercase mt-5 text-2xl duration-75 delay-50 hover:text-light hover:bg-lpeach bg-peach border-4 border-mossgreen focus:border-ltmossgreen shadow-xl">View all</a>
             </button>
             <br/>
         </div>
@@ -19,7 +19,7 @@ export default function Home() {
               <Image className='mx-auto md:float-right clearfix md:2/5 xl:h-1/3 md:w-4/12 xl:w-3/12 border-4 md:border-8 border-lmossgreen rounded-xl m-4 shadow-lg md:mr-16' src="/Image_M_1.jpeg" alt="Slipper with 3 diffrent colours, dark blue, grey and scarlett" width={500} height={500}/>
             </div>
             <h1 className="text-5xl lg:text-7xl font-bold text-center md:text-left md:ml-10 mt-12 lg:mt-20 underlinemain">Specialising in</h1>
-            <p className="text-3xl sm:text-4xl lg:text-5xl font-normal text-center md:text-left mt-4 mb-10 md:ml-10">Moccasin and Turn slippers and sheos</p>
+            <p className="text-3xl sm:text-4xl lg:text-5xl font-normal text-center md:text-left mt-4 mb-10 md:ml-10">Moccasins, Turn slippers and shoes</p>
             <p className="text-center md:text-left text-2xl sm:text-3xl sm:mt-5 ml-1 font-light px-8 mt-5 mb-10">Check out our showcase <a href="/showcase" className=' text-mossgreen hover:text-ltmossgreen'>here</a></p>
         </div>
 
